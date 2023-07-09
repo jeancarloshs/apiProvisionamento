7c38b9a695f0dd3c2fe7cda62450c22ead7c89a2
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2023-07-09 19:43:10 -0300

    Update server.js

--- server.js
+++ server.js
@@ -3,15 +3,6 @@ import app from "./src/app.js";
 // Aqui deixamos configurado para caso esteja em ambiente DEV use a porta 3000 ou a porta que está no .env
 const port = process.env.PORT || 3000;
 
-// Configuração do cabeçalho CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS, PATCH, DELETE, POST, PUT');
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
-
 // A porta que o servidor ira ouvir
 app.listen(port, () => {
     console.log(" ");
