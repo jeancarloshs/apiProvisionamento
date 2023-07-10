d56f5748dbb550bb1ad74479dde74e515a10b5ec
Jean <jeancarlos.na@hotmail.com>
2023-07-09 21:10:14 -0300

    removido cors

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ import app from "./src/app.js";
 // Aqui deixamos configurado para caso esteja em ambiente DEV use a porta 3000 ou a porta que está no .env
 const port = process.env.PORT || 3000;
 
-app.use(CorsOptions());
-
 // A porta que o servidor ira ouvir
 app.listen(port, () => {
     console.log(" ");
