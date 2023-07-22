2b459390a88d411046f6435a4bcaf12f9517512b
Jean Carlos <60233926+jeancarloshs@users.noreply.github.com>
2023-07-22 11:53:05 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import db from "./config/dbConfig.js";
 import routes from "./routes/routes.js";
+import cors from "cors";
 
 async function connectToDatabase() {
     try {
@@ -17,18 +18,20 @@ async function connectToDatabase() {
   
 const app = express();
 
+app.use(cors());
+
 // é um recurso do Express que vai conseguir fazer interpretar o que está chegando via post ou via put
 // e transformar aquilo em um objeto para eu poder armazenar, visualizar e manipular.
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*'); // Substitua pelo domínio do seu aplicativo front-end
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Adicione os métodos HTTP permitidos
-  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*'); // Substitua pelo domínio do seu aplicativo front-end
+//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS'); // Adicione os métodos HTTP permitidos
+//   res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+//   res.setHeader('Access-Control-Allow-Credentials', 'true');
+//   next();
+// });
 
 // routes(app);
 
