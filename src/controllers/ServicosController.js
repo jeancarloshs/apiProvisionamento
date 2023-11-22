5fc4e6cf95a98ab18f42a9593e84e59e1458e2c5
Jean <jeancarlos.na@hotmail.com>
2023-11-21 22:11:13 -0300

    hotfix/recuperada rotas que foram deletadas

--- src/controllers/ServicosController.js
+++ src/controllers/ServicosController.js
@@ -29,4 +29,93 @@ export default {
     }
     return res.json(response);
   },
+
+  async criarTipoDeServico(req, res) {
+    const response = { ...responseModel };
+    const dataAtual = new Date();
+    const { tipoDeServico } = req.body;
+    let query = "";
+
+    try {
+      query = await db`
+      INSERT INTO "tbTipoDeServico" ("tipoDeServico", "created_at", "update_at") 
+      VALUES (${tipoDeServico}, ${dataAtual}, NULL)
+      RETURNING *;`;
+
+      response.success = query.length > 0;
+
+      if (response.success) {
+        response.success = true;
+        response.found = query.length;
+        response.data = constants["201"].serviceCreatedSuccessfully;
+        return res.status(201).json(response);
+      } else {
+        response.error = constants["404"].noServiceFound;
+      }
+    } catch (error) {
+      console.error("ERROR:", error);
+      response.error = "Ocorreu um erro ao processar a solicitação.";
+      return res.status(500).json(response);
+    }
+
+    return res.json(response);
+  },
+
+  async atualizaTipoDeServico(req, res) {
+    const response = { ...responseModel };
+    const dataAtual = new Date();
+    const serviceId = req.params.id;
+    const { tipoDeServico } = req.body;
+    let query = "";
+
+    try {
+      query = await db`
+      UPDATE "tbTipoDeServico" SET "tipoDeServico"=${tipoDeServico}, "update_at"=${dataAtual} 
+      WHERE "id"=${serviceId}
+      RETURNING *;`;
+
+      response.success = query.length > 0;
+
+      if (response.success) {
+        response.success = true;
+        response.found = query.length;
+        response.data = constants["201"].serviceUpdateSuccess;
+      } else {
+        response.error = constants["404"].noServiceFound;
+        return res.status(404).json(response);
+      }
+    } catch (error) {
+      console.error("ERROR:", error);
+      response.error = constants["500"].errorOccurred;
+      return res.status(500).json(response);
+    }
+    return res.json(response);
+  },
+
+  async deletarTipoDeServico(req, res) {
+    const response = { ...responseModel };
+    const serviceId = req.params.id;
+    let query = "";
+
+    try {
+      query = await db`
+      DELETE FROM "tbTipoDeServico" WHERE "id"=${serviceId}
+      RETURNING *;`;
+
+      response.success = query.length > 0;
+      if (response.success) {
+        response.data = query.length;
+        response.found = query.length;
+        response.data = constants["200"].serviceDeleted;
+        return res.status(200).json(response);
+      } else {
+        response.error = constants["404"].noServiceFound;
+      }
+    } catch (error) {
+      console.error("ERROR:", error);
+      response.error = "Ocorreu um erro ao processar a solicitação.";
+      return res.status(500).json(response);
+    }
+    return res.json(response);
+  },
 };
