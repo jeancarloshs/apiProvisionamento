32d7919c60c383cea208c6cf635614c4a527e786
Jean Carlos <jeancarlos.na@hotmail.com>
2024-03-24 13:09:26 -0300

    adicionado funcionalidade para pegar informações do provedor

---
+++ src/models/ipsModel.js
@@ -0,0 +1,95 @@
+import { DataTypes, Sequelize } from "sequelize";
+import sequelize from "../config/dbConfig.js";
+import AppsModel from "./appsModel.js";
+import Users from "./usuariosModel.js";
+
+const IpsModel = sequelize.define("tbIps", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+    },
+    userId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: Users,
+            key: 'id'
+        }
+    },
+    userApp: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        references: {
+            model: AppsModel,
+            key: 'id',
+        }
+    },
+    countryName: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    countryCode: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    estadoAbreviado: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    estadoCompleto: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    cidadeNome: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    cepCidade: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    latitude: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    longitude: {
+        type: DataTypes.FLOAT,
+        allowNull: true
+    },
+    timezone: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    isp: {
+        type: DataTypes.STRING,
+        allowNull: true,
+    },
+    org: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    as: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    ipQuery: {
+        type: DataTypes.STRING,
+        allowNull: true
+    },
+    created_at: {
+        type: DataTypes.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP"),
+        allowNull: true,
+    },
+},
+    {
+        tableName: "tbIps",
+        freezeTableName: true
+    })
+
+IpsModel.belongsTo(Users, { foreignKey: "userId" })
+IpsModel.belongsTo(AppsModel, { foreignKey: "userApp" })
+
+export default IpsModel
---
+++ src/services/getIpService.js
@@ -0,0 +1,31 @@
+import IpsModel from "../models/ipsModel.js"
+
+export const getInfoIp = async (userId, userApp, ip) => {
+    await fetch(`http://ip-api.com/json/${ip}`, {
+        method: 'GET'
+    }).then(response => response.json()
+        .then(response => {
+            if (response.status !== 'fail') {
+                IpsModel.create({
+                    userId: userId,
+                    userApp: userApp,
+                    countryName: response.country,
+                    countryCode: response.countryCode,
+                    estadoAbreviado: response.region,
+                    estadoCompleto: response.regionName,
+                    cidadeNome: response.city,
+                    cepCidade: response.zip,
+                    latitude: response.lat,
+                    longitude: response.lon,
+                    timezone: response.timezone,
+                    isp: response.isp,
+                    org: response.org,
+                    as: response.as,
+                    ipQuery: response.query
+                })
+            }
+        }),
+    ).catch(error => {
+        console.error('Erro ao buscar os dados do IP:', error)
+    })
+}
