8c7073238b9c41cc81b83601a6d1b52c58b75020
Jean Carlos <jeancarlos.na@hotmail.com>
2024-05-25 18:37:01 -0300

    test: refatorado teste

--- src/test/functional/index.test.ts
+++ src/test/functional/index.test.ts
@@ -5,12 +5,36 @@ import express from 'express';
 import supertest from 'supertest';
 const app = express();
 
-describe('Test My app server', function () {
-  it('Should Get main route with just a few times', async () => {
-    const { body, status } = await supertest(app)
+// DESCRIBE -> bloco de testes - tests suites
+// IT or TEST -> declara um unico teste unitario - tests cases
+// EXPECT-> asserções do resultado - validar resultados
+
+describe('Test My app server', () => {
+  test('Should Get main route with just a few times', async () => {
+    const { body, status } = await supertest(server)
       .get('/');
     expect(status).toBe(200);
-    // expect(body).toBe({ message: "Server is running" });
     expect(body).toHaveProperty('message');
   })
 });
+
+describe('Test Login Route', () => {
+  test('Should return a token', async () => {
+    const request = {
+      body: {
+        "email": "????",
+        "password": "????"
+      }
+    }
+
+    const sent = {
+      code: jest.fn()
+    }
+
+    console.log('req', sent)
+    // const { body, status } = await supertest(server)
+    //   .post('/login');
+    // expect(status).toBe(200);
+    // expect(body).toHaveProperty('token');
+  });
+})
